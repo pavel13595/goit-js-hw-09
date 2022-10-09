df0904e0dfd9f5ef87b641f7750b46b3f8c67c98
pavel13595 <69019724+pavel13595@users.noreply.github.com>
2022-10-09 08:50:46 +0000

    Deploying to gh-pages from @ pavel13595/goit-js-hw-09@c5fb74d6c4ab46b541880abfcea2544b75d7f1c7 🚀

---
+++ 01-color-switcher.2642ee8d.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){e=setInterval(n,1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearTimeout(e),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));let e=null;function n(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}
+//# sourceMappingURL=01-color-switcher.2642ee8d.js.map
